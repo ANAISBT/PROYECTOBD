eed1dbda2f7a3a3283be3ca7175936082811f210
ANAIS <anais.bustamante@unmsm.edu.pe>
2023-03-09 11:53:13 -0500

    db

--- database/db.sql
+++ database/db.sql
@@ -15,6 +15,15 @@ CREATE TABLE donantes (
   telefono VARCHAR(15) NOT NULL
 );
 
+CREATE TABLE solicitudes (
+  id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  nombre VARCHAR(30) NOT NULL,
+  motivo VARCHAR(50) NOT NULL,
+  grupo VARCHAR(15) NOT NULL,
+  RH VARCHAR(2) NOT NULL,
+  cantidad INT(3) NOT NULL
+);
+
 CREATE TABLE extracciones (
   id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
   fecha DATE NOT NULL,
@@ -23,6 +32,85 @@ CREATE TABLE extracciones (
   grupo VARCHAR(15) NOT NULL,
   RH VARCHAR(2) NOT NULL
 );
+
+CREATE TABLE inventario (
+  id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  volumen INT(3) NOT NULL,
+  grupo VARCHAR(15) NOT NULL,
+  RH VARCHAR(2) NOT NULL
+);
+
+CREATE TABLE citas(
+  id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  Nombre VARCHAR(15) NOT NULL,
+  Apellido VARCHAR(15) NOT NULL,
+  correo VARCHAR(15) NOT NULL,
+  fecha date NOT NULL,
+  hora text not null
+);
+
+CREATE VIEW sangre_disponibles AS
+SELECT grupo, RH, SUM(volumen) AS volumen_total FROM extracciones GROUP BY grupo, RH;
+
+DELIMITER //
+CREATE PROCEDURE calcular_edad(IN fecha_nacimiento DATE, OUT edad INT)
+BEGIN
+    SET edad = TIMESTAMPDIFF(YEAR, fecha_nacimiento, CURDATE());
+END //
+DELIMITER ;
+
+DELIMITER //
+CREATE TRIGGER actualizar_edad_donante
+BEFORE INSERT ON donantes
+FOR EACH ROW
+BEGIN
+    CALL calcular_edad(NEW.fechaNacimiento, @edad);
+    SET NEW.edad = @edad;
+END //
+DELIMITER ;
+
+
+DELIMITER $$
+CREATE PROCEDURE actualizar_inventario()
+BEGIN
+    DECLARE done INT DEFAULT FALSE;
+    DECLARE grupo_sanguineo VARCHAR(15);
+    DECLARE RH VARCHAR(2);
+    DECLARE volumen_total INT(3);
+    
+    -- Cursor para recorrer las extracciones
+    DECLARE cur_extracciones CURSOR FOR SELECT grupo, RH, volumen FROM extracciones;
+    DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
+    
+    -- Abrir el cursor
+    OPEN cur_extracciones;
+    
+    -- Recorrer las extracciones
+    WHILE NOT done DO
+        FETCH cur_extracciones INTO grupo_sanguineo, RH, volumen_total;
+        
+        -- Actualizar el inventario
+        UPDATE inventario SET volumen = volumen + volumen_total WHERE grupo = grupo_sanguineo AND RH = RH;
+    END WHILE;
+    
+    -- Cerrar el cursor
+    CLOSE cur_extracciones;
+END$$
+DELIMITER ;
+
+
+
+
+DELIMITER $$
+CREATE TRIGGER actualizar_inventario AFTER INSERT ON extracciones
+FOR EACH ROW
+BEGIN
+    CALL actualizar_inventario();
+END$$
+DELIMITER ;
+
+
+CREATE INDEX idx_grupoSolicitud ON solicitudes(grupo);
 
 -- to show all tables
 show tables;
